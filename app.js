5e58a99128489ab84871f181dfbfe1c85c94b1dd
Kyle Combs <kylecombs429@gmail.com>
2021-06-25 13:12:21 -0400

    initial diretory

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const port = 8080
+
+app.use(express.static('public'))
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`)
+  })
