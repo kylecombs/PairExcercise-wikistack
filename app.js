d4b8a1b6ef27155e81871a45914fa4507a2de01b
Kyle Combs <kylecombs429@gmail.com>
2021-06-25 15:12:14 -0400

    setup complete

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express()
+const { db } = require('./models');
 const port = 8080
 
 app.use(express.static('public'))
 
-
-
-
-
-
-
-
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
